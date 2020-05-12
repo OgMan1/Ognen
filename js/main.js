ad8c25ec8ba5b89d2e552b453fdbfd40377e88fa
OgnenManchevski <46723553+OgMan1@users.noreply.github.com>
2020-05-12 23:27:16 +0200

    Add files via upload
    updated

--- js/main.js
+++ js/main.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
+$(document).ready(function () {
   AOS.init({
-    duration: 1200
+    duration: 1200,
   });
 
   $("#myVideo").attr("src", "vid/Sky.mp4");
-  $(".imgSecondRow2").on("mouseenter", function() {
+  $(".imgSecondRow2").on("mouseenter", function () {
     $(".imgSecondRow2").attr("src", "img/logo_transparent5.png");
   });
-  $(".imgSecondRow2").on("mouseleave", function() {
+  $(".imgSecondRow2").on("mouseleave", function () {
     $(".imgSecondRow2")
       .attr("src", "img/logoNewTrans.png")
       .css("transition", "transform 0.5s linear");
   });
-  $(".h1Flex").on("mouseenter", function() {
+  $(".h1Flex").on("mouseenter", function () {
     $(".h1Flex").css({
       transform: "scale(1.15)",
-      transition: "transform 0.5s linear"
+      transition: "transform 0.5s linear",
     });
     $(".spH1").css("display", "block");
   });
-  $(".h1Flex").on("mouseleave", function() {
+  $(".h1Flex").on("mouseleave", function () {
     $(".h1Flex").css({
       transform: "skewY(-10deg)",
-      transition: "transform 0.5s linear"
+      transition: "transform 0.5s linear",
     });
     $(".spH1").css("display", "none");
   });
 
-  $(".imgPhone").on("mouseenter", function() {
-    $(".imgPhone").addClass("shake");
-  });
+  // $(".imgPhone").on("mouseenter", function() {
+  //   $(".imgPhone").addClass("shake");
+  // });
 
-  $(".imgPhone").on("mouseleave", function() {
-    $(".imgPhone").removeClass("shake");
-  });
+  // $(".imgPhone").on("mouseleave", function() {
+  //   $(".imgPhone").removeClass("shake");
+  // });
 
-  $(".imgOg").on("mouseenter", function() {
+  $(".imgOg").on("mouseenter", function () {
     $(".imgOg").addClass("flicker-3");
   });
 
-  $(".imgOg").on("mouseleave", function() {
+  $(".imgOg").on("mouseleave", function () {
     $(".imgOg").removeClass("flicker-3");
   });
 
-  $(".fa-music").on("click", function() {
+  $(".fa-music").on("click", function () {
     $(".audioPlayer").trigger("play");
   });
 
   let playing = false;
 
-  $(".fa-music").click(function() {
+  $(".fa-music").click(function () {
     $(this).toggleClass("fa-pause");
 
     if (playing == false) {
@@ -63,7 +63,7 @@ $(document).ready(function() {
     }
   });
 
-  $("#test").on("click", function() {
+  $("#test").on("click", function () {
     let doc = document.getElementsByClassName("fa-moon");
     if (doc.length > 0) {
       moon();
@@ -91,25 +91,21 @@ $(document).ready(function() {
     $("textarea, input.form-control, .textarea:focus").css({
       color: "aqua",
       border: "1px solid orange",
-      background: "transparent"
+      background: "transparent",
     });
     $(".textarea:focus").css("color", "purple");
     $(".btnForm, .btn-danger, .navText").addClass("bg-warning");
     $(".btnForm, .btnFor, .progBarInfo, .navText").css({
       "font-weight": "700",
-      color: "rgb(51, 0, 51)"
+      color: "rgb(51, 0, 51)",
     });
-    $(".progBarInfo")
-      .removeClass("bg-danger")
-      .addClass("bg-warning");
-    $(".fa-moon")
-      .removeClass("fa-moon")
-      .addClass("fa-sun");
+    $(".progBarInfo").removeClass("bg-danger").addClass("bg-warning");
+    $(".fa-moon").removeClass("fa-moon").addClass("fa-sun");
     $(".imgSecondRow2").attr("src", "img/logo_transparent.png");
-    $(".imgSecondRow2").on("mouseenter", function() {
+    $(".imgSecondRow2").on("mouseenter", function () {
       $(".imgSecondRow2").attr("src", "img/logo_transparent3.png");
     });
-    $(".imgSecondRow2").on("mouseleave", function() {
+    $(".imgSecondRow2").on("mouseleave", function () {
       $(".imgSecondRow2")
         .attr("src", "img/logo_transparent.png")
         .css("transition", "transform 0.5s linear");
@@ -126,9 +122,7 @@ $(document).ready(function() {
       ".h1Flex, .secondRow h2, .thirdRow h2, .cardRow h5, .cardRow ul, #ogSpan"
     ).css("color", "red");
     $(".h1Flex").removeClass("h1FlexDark");
-    $(".progBarInfo")
-      .removeClass("bg-info")
-      .addClass("bg-danger");
+    $(".progBarInfo").removeClass("bg-info").addClass("bg-danger");
     $("input.form-control, .cardRow ul li").css({ color: "rgb(51, 0, 51)" });
     $(".btnForm, .btn-danger, .navText").removeClass("bg-warning");
     $(".btnForm, .btnFor, .progBarInfo").css("color", "white");
@@ -138,18 +132,16 @@ $(document).ready(function() {
     $(".navText").css("background-color", "transparent");
     $("textarea, input.form-control, .textarea:focus").css({
       border: "1px solid red",
-      color: "rgb(51, 0, 51)"
+      color: "rgb(51, 0, 51)",
     });
     $(".logoPic, .logoPicFoot").attr("src", "img/logo.png");
-    $(".fa-sun")
-      .removeClass("fa-sun")
-      .addClass("fa-moon");
+    $(".fa-sun").removeClass("fa-sun").addClass("fa-moon");
     $(".imgSecondRow2").attr("src", "img/logoNewTrans.png");
     $("footer").css("color", "rgb(51, 0, 51)");
-    $(".imgSecondRow2").on("mouseenter", function() {
+    $(".imgSecondRow2").on("mouseenter", function () {
       $(".imgSecondRow2").attr("src", "img/logo_transparent5.png");
     });
-    $(".imgSecondRow2").on("mouseleave", function() {
+    $(".imgSecondRow2").on("mouseleave", function () {
       $(".imgSecondRow2")
         .attr("src", "img/logoNewTrans.png")
         .css("transition", "transform 0.5s linear");
